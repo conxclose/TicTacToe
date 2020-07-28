5d4fc3b744e1c5073efe3010851e0d3a03ae3cdc
Con <conxclose@gmail.com>
2020-07-28 12:07:10 +0100

    Basic gameboard with array and basic mutator functions
    2D gameboard using Unity UI elements in reference to a 3x3 byte array, created some basic mutator functions to test and further expand

---
+++ TicTacToe/Assets/Scripts/Gameboard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.Experimental.ParticleSystemJobs;
+
+public class Gameboard : MonoBehaviour
+{
+    private byte[,] boardArray;
+
+    private const byte empty = 0;
+    private const byte x = 1;
+    private const byte o = 2;
+
+    bool isEmpty = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boardArray = new byte[3, 3] {
+            {0, 1, 2,} ,
+            {3, 4, 5,} ,
+            {6, 7, 8}
+        };
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                boardArray[i, j] = empty;
+            }
+        }
+    }
+
+    void UpdateGameboard(byte xPos, byte yPos, byte symbol)
+    {
+        if (symbol == x)
+            boardArray[xPos, yPos] = x;
+        else
+            boardArray[xPos, yPos] = o;
+    }
+
+    public bool CheckSpaceIsEmpty(byte xPos, byte yPos)
+    {
+        if (boardArray[xPos, yPos] == empty)
+        {
+            isEmpty = true;
+            return isEmpty;
+        }
+        else
+            return isEmpty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TicTacToe/Assets/Scripts/PlayerFunctionality.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerFunctionality : MonoBehaviour
+{
+    public GameObject gameBoard;
+    private Gameboard gb;
+    public Button b;
+    public Text t;
+    public string playerIndentity;
+
+    void Start()
+    {
+       gb = gameBoard.GetComponent<Gameboard>();
+    }
+
+    public void SetSpace()
+    {
+        if (gb.CheckSpaceIsEmpty(0, 0))
+        {
+            Debug.Log("EMPTY");
+            t.text = playerIndentity;
+            b.interactable = false;
+        }
+    }
+}
